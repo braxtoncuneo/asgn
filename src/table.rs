cd2359d60dced2b51cbe12aca232eef25eeec855
JavaSux <javasux0@gmail.com>
2023-12-30 21:42:54 -0800

    Rework Table API

--- src/table.rs
+++ src/table.rs
@@ -1,8 +1,17 @@
 use core::fmt;
 
-use crate::error::Error;
+use crate::error;
 use colored::Colorize;
 use itertools::Itertools;
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct Error;
+
+impl From<Error> for error::Error {
+    fn from(_: Error) -> Self {
+        error::Error::TableError
+    }
+}
 
 pub struct Table {
     width: usize,
@@ -10,38 +19,38 @@ pub struct Table {
 }
 
 impl Table {
-    pub fn new(width: usize) -> Self {
+    pub const NONE_REPR: &'static str = "NONE";
+
+    pub fn new(header: impl Into<Box<[String]>>) -> Self {
+        let header = header.into();
         Table {
-            width,
-            rows: Vec::new(),
+            width: header.len(),
+            rows: vec![header],
         }
     }
 
-    pub fn add_row(&mut self, row: impl Into<Box<[String]>>) -> Result<(), Error> {
-        let row = row.into();
+    pub fn extend<Row: Into<Box<[String]>>, I: IntoIterator<Item=Row>>(&mut self, rows: I) -> Result<(), Error> {
+        let rows = rows.into_iter();
+        self.rows.reserve(rows.size_hint().0);
 
-        if row.len() != self.width {
-            return Err(Error::Custom(
-                "Internal Error: Attempted to add a row to a table with a different width".to_owned(),
-                "Contact the instructor.".to_owned()
-            ));
-        }
-
-        self.rows.push(row);
-        Ok(())
-    }
-
-    fn as_table_row(row: &[String], col_widths: &[usize]) -> String {
-        row.iter()
-            .zip(col_widths)
-            .map(|(text, width)| format!(" {text:width$} "))
-            .join("|")
+        rows.map(Row::into).try_for_each(|row|
+            if row.len() == self.width {
+                self.rows.push(row);
+                Ok(())
+            } else {
+                Err(Error)
+            }
+        )
     }
 
     pub fn as_csv(&self) -> String {
         self.rows.iter()
             .map(|row| row.iter().join(","))
             .join("\n")
+    }
+
+    pub fn option_repr(value: Option<impl fmt::Display>) -> String {
+        value.as_ref().map(ToString::to_string).unwrap_or_else(|| Self::NONE_REPR.to_owned())
     }
 }
 
@@ -55,7 +64,11 @@ impl fmt::Display for Table {
             .collect();
 
         for (i, row) in self.rows.iter().enumerate() {
-            let text = Self::as_table_row(row, &widths);
+            let text = row.iter()
+                .zip(&widths)
+                .map(|(text, width)| format!(" {text:width$} "))
+                .join("|");
+
             let styled = match i {
                 0 => text.reversed().to_string(),
                 i if i % 2 == 0 => text.on_bright_black().to_string(),
